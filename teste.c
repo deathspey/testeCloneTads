46a90a56405446f8fd669634d0b07a6bb3ca7ce7
Caio Vianeda <leandropontaltkd2012@hotmail.com>
2020-02-14 10:13:42 -0300

    sei verificar par e impar

--- teste.c
+++ teste.c
@@ -3,6 +3,17 @@
 
 int main(){
 
+    int valor;
+
+    printf("\ndigite um valor:");
+    scanf("%i", &valor);
+
+    if(valor%2==1){
+
+        printf("\nValor impar\n");
+    }
+
+    else printf("\nValor par\n");
 
     return 0;
 }
